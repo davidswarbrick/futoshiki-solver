21dd3147ef2dc55b8145c72c772a918c3a8b270a
David Swarbrick <david.swarbrick9@gmail.com>
2020-07-23 00:21:05 +0100

    Finding corners of boxes for Futoshiki Puzzle
    Adjusted accuracy of polydp to 3%, using Canny threshold of 50
    from example, with 2.5x ratio used in previous OpenCV testing.
    
    "Square" class wraps points to provide easy access to area,
    can be discarded if not useful later.
    
    To find the puzzle alone, it would be best to compare relative
    areas and locations of corners to find 5 x 5 grid of similarly
    sized squares. Once grid has been found, camera orientation can
    be calculated to map square contents onto a flat plane for number
    recognition (using ML trained on MNIST etc.)
    
    Canny, PolyDP and Cosine-angle-checking thresholds might need to be
    adjusted for more obtuse-angled photos.

--- squares.py
+++ squares.py
@@ -5,7 +5,20 @@ import matplotlib.pyplot as plt
 # Square detector copied from C++ source code here: https://docs.opencv.org/master/db/d00/samples_2cpp_2squares_8cpp-example.html
 
 
+class Square:
+    def __init__(self, corners):
+        self.corners = corners
+
+    @property
+    def area(self):
+        return cv2.contourArea(self.corners)
+
+
 def is_square(approx):
+    """
+    Check the 4 points in the approximate contour form a rectangle
+    This is completed checking the cosine of each corner angle
+    """
     max_cosine = 0
     for j in range(2, 5):
         a = approx[j % 4]
@@ -18,47 +31,37 @@ def is_square(approx):
     return max_cosine < 0.3
 
 
-def find_squares(img, N=11, thresh=50):
+def draw_square(img, square):
+    return cv2.polylines(img, square, True, (255, 0, 0), 5)
+
+
+def find_squares(img, thresh=50, ratio=2.5):
     low = cv2.pyrDown(img)
     smoothed = cv2.pyrUp(low)
     squares = []
     # Assuming black & white image (as we are detecting black squares on grey paper)
-    for l in range(N):
-        # l is the threshold level
-        if l == 0:
-            # Use Canny for zero level to catch squares with gradient shading
-            # Upper threshold thresh, lower to 0 forces edges merging
-            gray0 = cv2.Canny(smoothed, 0, thresh, 5)
-            # dilate Canny output to remove potential holes between edge segments
-            # squares.cpp uses an empty kernel so we attempt an empty numpy matrix as kernel
-            gray = cv2.dilate(gray0, np.ones((3, 3), np.uint8))
-        else:
-            # gray = gray0[gray0 >= (l + 1) * 255 / N]
-            t = (l + 1) * 255 / N
-            print(l, ": Threshold pixel value: ", t)
-            retval, gray = cv2.threshold(gray0, t, 255, cv2.THRESH_TOZERO)
-        contours, hierarchy = cv2.findContours(
-            gray, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE
+
+    gray = cv2.Canny(smoothed, thresh, ratio * thresh)
+    contours, hierarchy = cv2.findContours(gray, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    squares_found = False
+    for i in range(len(contours)):
+        # Approximate contour with accuracy proportional to perimeter
+        approx = cv2.approxPolyDP(
+            contours[i], cv2.arcLength(contours[i], True) * 0.03, True
         )
-        squares_found = False
-        for i in range(len(contours)):
-            # Approximate contour with accuracy proportional to perimeter
-            approx = cv2.approxPolyDP(
-                contours[i], cv2.arcLength(contours[i], True) * 0.02, True
-            )
-            # square contours should have 4 vertices after approximation
-            # relatively large area (to filter out noisy contours) and be convex
-            if (
-                len(approx) == 4
-                and cv2.contourArea(approx) > 1000
-                and cv2.isContourConvex(approx)
-                and is_square(approx)
-            ):
-                squares_found = True
-                squares.append(approx)
-        if squares_found:
-            plt.figure()
-            plt.imshow(gray, "gray")
+        # square contours should have 4 vertices after approximation
+        # relatively large area (to filter out noisy contours) and be convex
+        if (
+            len(approx) == 4
+            # and cv2.contourArea(approx) > 1000
+            and cv2.isContourConvex(approx)
+            and is_square(approx)
+        ):
+            squares_found = True
+            squares.append(Square(approx))
+    # if squares_found:
+    #    plt.figure()
+    #    plt.imshow(gray, "gray")
 
     return squares
 
@@ -70,8 +73,14 @@ for i in range(11):
 
 # p = cv2.imread("img/3712crop.jpg",0)
 p = imgs[2]
-
-plt.imshow(p, "gray")
-N = 2
 squares = find_squares(p)
+
+plt.figure(1)
+plt.subplot(121)
+plt.imshow(p, "gray")
+for s in squares:
+    p = draw_square(p, s.corners)
+plt.subplot(122)
+plt.imshow(p)
+print("Found {} squares".format(len(squares)))
 plt.show()
